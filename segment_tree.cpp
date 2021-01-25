c60e09ba3da69a2389596512415d31c6ff738593
Rawan <tahsan11.haq@gmail.com>
2021-01-25 17:35:06 +0600

    Update of segment tree

---
+++ segment_tree.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Segtree {
+    int size;
+    vector<long long> sums;
+    
+    void init(int n) {
+        size = 1;
+        while (size < n) {
+            size *= 2;
+        }
+        sums.assign(size*2, 0LL);
+    }
+    void build(vector<long long> &leaves, int x, int lx, int rx) {
+        
+        if (rx - lx == 1) {
+            if (lx < leaves.size()) sums[x] = leaves[lx];
+            return;
+        }
+        int m = (lx+rx)/2;
+        build(leaves, 2*x+1, lx, m);
+        build(leaves, 2*x+2, m, rx);
+        sums[x] = sums[2*x+1] + sums[2*x+2];
+    }
+    void build(vector<long long> &leaves) {
+        build(leaves, 0, 0, size);
+    }
+    
+    void print_segtree(){
+        for(int i = 0; i < log2(sums.size()); i++) {
+            for (int j = int(pow(2,i)-1); j < int(pow(2,i))+int(pow(2,i))-1; j++)
+                cout << sums[j] << " ";
+            cout << endl;
+        }
+    }
+    void set(int i, int x, int lx, int rx, long long v) {
+        if (i >= rx || i < lx) return;
+        if (rx - lx == 1) {
+            sums[x] = v;
+            return;
+        }
+        int m = (lx+rx)/2;
+        if (i < m)
+            set(i, 2*x+1, lx, m, v);
+        else
+            set(i, 2*x+2, m, rx, v);
+        sums[x] = sums[2*x+1] + sums[2*x+2];
+    }
+    void set(int i, long long v) {
+        set(i, 0, 0, size, v);
+    }
+    
+    long long sum(int x, int lx, int rx, int l, int r) {
+        if (r <= lx || rx <= l) return 0;
+        if (l <= lx && r >= rx) return sums[x];
+        int m = (lx+rx)/2;
+        return sum(2*x+1, lx, m, l, r)+sum(2*x+2, m, rx, l, r);
+    }
+    
+    long long sum(int l, int r) {
+        return sum(0, 0, size, l, r);
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<long long> leaves(n);
+    for (int i = 0; i < n; i++) {
+        cin >> leaves[i];
+    }
+    Segtree st;
+    st.init(n);
+    st.build(leaves);
+    for (int i = 0; i < m; i++) {
+        int k;
+        cin >> k;
+        if (k == 1) {
+            int l;
+            long long v;
+            cin >> l;
+            cin >> v;
+            st.set(l, v);
+        }
+        else {
+            int l, r;
+            cin >> l >> r;
+            cout << st.sum(l, r) << endl;
+        }
+    }
+}
